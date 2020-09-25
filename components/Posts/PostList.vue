88543f25f961ab4eeab4691e4b77556b02a6a89a
Akinori Sakakibara <gorion.1213@gmail.com>
2020-09-25 21:47:11 +0900

    created the blog

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="post-list">
+      <PostPreview 
+        id="1"
+        :is-admin="isAdmin"
+        thumbnail="http://4.bp.blogspot.com/-kBsKTbWxCRQ/T9TRVCRGqoI/AAAAAAAAKRQ/ESK-JCzJiWM/s1600/[wallcoo_com]_3528_future_TECH_Image.jpg"
+        title="Hello there!"
+        previewText="This my first post!" />
+      <PostPreview 
+        id="2"
+        :is-admin="isAdmin"
+        thumbnail="http://4.bp.blogspot.com/-kBsKTbWxCRQ/T9TRVCRGqoI/AAAAAAAAKRQ/ESK-JCzJiWM/s1600/[wallcoo_com]_3528_future_TECH_Image.jpg"
+        title="Hello there - the second time!"
+        previewText="This my second post!" />
+      <PostPreview 
+        id="3"
+        :is-admin="isAdmin"
+        thumbnail="http://4.bp.blogspot.com/-kBsKTbWxCRQ/T9TRVCRGqoI/AAAAAAAAKRQ/ESK-JCzJiWM/s1600/[wallcoo_com]_3528_future_TECH_Image.jpg"
+        title="Hello there - the third time!"
+        previewText="This my third post!" />
+    </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Akinori',
+        title: 'My awesome Post',
+        content: 'Super amazing thanks for that!',
+        thumbnailLink: 'http://4.bp.blogspot.com/-kBsKTbWxCRQ/T9TRVCRGqoI/AAAAAAAAKRQ/ESK-JCzJiWM/s1600/[wallcoo_com]_3528_future_TECH_Image.jpg'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .update-form {
+    width: 90%;
+    margin: 20px auto;
+  }
+
+  @media (min-width: 768px) {
+    .update-form {
+      width: 500px;
+    }
+  }
+</style>
